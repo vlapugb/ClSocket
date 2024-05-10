979b7e7121bc39183608f8f5ef5591f08abdc651
vlapugb <vladimirpugovkinspb@gmail.com>
2024-05-10 11:07:50 +0300

    updated 1.0

---
+++ Headers/src/requests.h
@@ -0,0 +1,21 @@
+#include "PassiveSocket.h"
+#include "requests.cpp"
+void ShowError(CSimpleSocket ss, string s);
+
+void get_API_version (CActiveSocket& SocketActive);
+
+void get_sw_revision(CActiveSocket& SocketActive);
+
+void get_gru_state(CActiveSocket& SocketActive);
+
+void socket_close(CActiveSocket SocketActive);
+
+vector <int32_t> get_unloaded_num(vector<int32_t> segment_status);
+
+void upload_segment(CActiveSocket &SocketActive, int32_t seg_num, bool need_confirm, vector<vector<int32_t>> nodes);
+
+void upload_traj(CActiveSocket& SocketActive, vector<vector<int32_t>> nodes);
+
+vector <int32_t> download_traject (CActiveSocket &SocketActive, int32_t points_cnt);
+
+vector<vector<int32_t>> get_nodes(const string& Traject_file_name);
